81613cc0852e761908737d6c650ca34e40e8a938
Gal <galbarda.dev@gmail.com>
2022-12-07 15:38:32 +0200

    Read info a list the amount each elf is holding and print

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -1,4 +1,10 @@
 """
 https://adventofcode.com/2022/day/1
+Day 01 ex.1 - Sum up the total calories for each elf and return the highest amount.
 """
 
+with open("day_01/input_01.txt") as file_hnd:
+    elves = file_hnd.read().split("\n\n")
+
+for elf in elves:
+    print(f"elf -----------\n{elf}")
