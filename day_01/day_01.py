b90032c02378652362229670192071a8cf0ce46d
Gal <galbarda.dev@gmail.com>
2022-12-07 16:07:23 +0200

    Add sumCalories function and print details on calories information

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -6,5 +6,19 @@ Day 01 ex.1 - Sum up the total calories for each elf and return the highest amou
 with open("day_01/input_01.txt") as file_hnd:
     elves = file_hnd.read().split("\n\n")
 
-for elf in elves:
+
+def sumCalories(elves):
+    """Return a list of the sums of the calories each elf is carrying"""
+
+    totalCalories = list()
+    for elf in elves: # map each elf calories into a list of ints before making a sum.
+        calories = map(int, elf.split())
+        totalCalories.append(sum(calories))
+    
+    return totalCalories
+
+totalCalories = sumCalories(elves)
+for i, elf in enumerate(elves, 0):
     print(f"elf -----------\n{elf}")
+    print(f"Total calories : {totalCalories[i]}")
+print(f"Highest calories carried: {max(totalCalories)}")
